bf439b60b2e0d8f12c8b226469e7f63a337eb381
atomozero <atomozero@users.noreply.github.com>
2026-06-04 19:29:11 +0200

    Remove build artifacts from libwebcam, add .gitignore

--- lib/libwebcam/objects.x86_64-cc13-release/AudioConsumer.d
+++
@@ -1,6 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc13-release/AudioConsumer.o : include/ErrorUtils.h \
-    ../../src/webcam/AudioConsumer.h include/WebcamLog.h \
-    ../../src/webcam/AudioSink.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
--- lib/libwebcam/objects.x86_64-cc13-release/USBVideoParser.d
+++
@@ -1,5 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86_64-cc13-release/USBVideoParser.o : include/ErrorUtils.h \
-    ../../src/webcam/USBVideoParser.h include/WebcamLog.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
